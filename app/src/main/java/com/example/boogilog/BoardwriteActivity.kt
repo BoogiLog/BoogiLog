ac0963bc9f53fc0fc440f35b28baa834bafa5cd6
sy_01 <92142899+Yangseyeong@users.noreply.github.com>
2022-11-21 14:28:49 +0900

    v

--- app/src/main/java/com/example/boogilog/BoardwriteActivity.kt
+++ app/src/main/java/com/example/boogilog/BoardwriteActivity.kt
@@ -2,17 +2,20 @@ package com.example.boogilog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil.setContentView
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.example.boogilog.databinding.Activity
+
 
 class BoardwriteActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityBoardWriteBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_boardwrite2)
+        setContentView(R.layout.fragment_post)
 
-        val database = Firebase.database
 
-        val myRef = database.getReference("Documents")
-        myRef.child("test").push().setValue(".")
-    }
 }
---
+++ app/src/main/java/com/example/boogilog/util/FBAuth.kt
@@ -0,0 +1,21 @@
+package com.example.boogilog.util
+
+import com.google.firebase.auth.FirebaseAuth
+
+class FBAuth {
+
+    companion object {
+
+        private lateinit var auth : FirebaseAuth
+
+        fun getUid() : String {
+
+            auth = FirebaseAuth.getInstance()
+
+            return auth.currentUser?.uid.toString()
+
+        }
+
+    }
+
+}
